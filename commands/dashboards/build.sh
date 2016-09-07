568f77016014881e19f0f54faf97aefce429791d
Simone Sciarrati <simone.sciarrati@meltwater.com>
2016-09-07 17:44:39 +0200

    add datasources/dashboard

--- commands/dashboards/build.sh
+++ commands/dashboards/build.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
-
+echo "Logging in to grafana"
 COOKIE_JAR=$(mktemp)
-curl -H 'Content-Type: application/json;charset=UTF-8' \
+sleep 5
+result=`curl -H 'Content-Type: application/json;charset=UTF-8' \
     --data-binary '{"user":"admin","email":"","password":"admin"}' \
     --cookie-jar "$COOKIE_JAR" \
-    'http://localhost:3000/login' > /dev/null 2>&1
+    'http://localhost:3000/login' > /dev/null 2>&1`
 
-curl -XPOST -i --cookie "$COOKIE_JAR" http://localhost:3000/api/datasources --data-binary @/vagrant/commands/dashboards/grafana_datasources.json -H "Content-Type: application/json"
+echo "Adding the datasource prometheus"
+sleep 3
+result=`curl -XPOST -i --cookie "$COOKIE_JAR" http://localhost:3000/api/datasources --data-binary @/vagrant/commands/dashboards/grafana_datasources.json -H "Content-Type: application/json"`
+if [ $? -ne 0 ];then
+  echo "Failed to update data sources"
+fi
+
 
 # TODO: This doesn't work:
-curl -XPOST -i --cookie "$COOKIE_JAR" http://localhost:3000/api/dashboards/db --data-binary @/vagrant/commands/dashboards/grafana_cadvisor.json -H "Content-Type: application/json"
-curl -XPOST -i --cookie "$COOKIE_JAR" http://localhost:3000/api/dashboards/db --data-binary @/vagrant/commands/dashboards/grafana_prometheus.json -H "Content-Type: application/json"
+#curl -XPOST -i --cookie "$COOKIE_JAR" http://localhost:3000/api/dashboards/db --data-binary @/vagrant/commands/dashboards/grafana_cadvisor.json -H "Content-Type: application/json"
+#curl -XPOST -i --cookie "$COOKIE_JAR" http://localhost:3000/api/dashboards/db --data-binary @/vagrant/commands/dashboards/grafana_prometheus.json -H "Content-Type: application/json"
